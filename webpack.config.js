966ae15274ba322609298b04425f066be7365364
Mlanawo MBECHEZI <mlanawo.mbechezi@alpacode.io>
2025-06-06 20:52:18 +0300

    Add postcss & autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", 'postcss-loader'],
             },
         ],
     },
